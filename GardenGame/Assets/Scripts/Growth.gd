eb2810411f99bdf3c0d36c66e835f10a1a17961f
Woody Wyche <woodywyche@greenestpastures.com.au>
2023-05-17 11:05:04 +0800

    Working on Growth Script

--- GardenGame/Assets/Scripts/Growth.gd
+++ GardenGame/Assets/Scripts/Growth.gd
@@ -1,12 +1,16 @@
 extends AnimatedSprite2D
 
 @export var growthTime = 20
+var growthPhases
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var curAnim = animation
+	growthPhases = sprite_frames.get_frame_count(curAnim)
+	print(growthPhases)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+	
