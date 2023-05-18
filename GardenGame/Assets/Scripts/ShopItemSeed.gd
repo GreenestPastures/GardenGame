35ecdd8e95427950d992fdfeb054f7b3abaa8350
Woody Wyche <woodywyche@greenestpastures.com.au>
2023-05-18 14:02:22 +0800

    Shop Item added
    Added shop items
    Assign plant by number
    Autoupdates graphic

--- GardenGame/Assets/Scripts/ShopItemSeed.gd
+++ GardenGame/Assets/Scripts/ShopItemSeed.gd
@@ -1,11 +1,14 @@
 extends Sprite2D
 
+signal chosenItem(seedName, seedTexture)
+
 @export var seed4Sale = 1
-@export var seedNames = []
+@export var seedNames: Array[String]
 @export var seedPic: Array[AtlasTexture]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	$Fruit.texture = seedPic[seed4Sale-1]
 	pass # Replace with function body.
 
 
