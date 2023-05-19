e985733966546b603f4d073cb3d267af0955775d
Woody Wyche <woodywyche@greenestpastures.com.au>
2023-05-19 16:19:39 +0800

    Scripting for HUD objects

--- GardenGame/Assets/Scripts/Statistics.gd
+++ GardenGame/Assets/Scripts/Statistics.gd
@@ -3,11 +3,13 @@ extends Node
 @export var beetroot = 3
 @export var cauliflower = 0
 @export var garlic = 0
+@export var lettuce = 0
 @export var mangocarrot = 0
 @export var milkroot = 0
 @export var potato = 0
-@export var radish = 0
+@export var radish = 6
 @export var tomato = 0
+
 
 
 # Called when the node enters the scene tree for the first time.
