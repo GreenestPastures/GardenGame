7c6f83a84587ba30e3e6cc1f04f2cbad2b27f073
Woody Wyche <woodywyche@greenestpastures.com.au>
2023-05-19 13:56:11 +0800

    Added Stats
    Added stats and a seed counter

---
+++ GardenGame/Assets/Scripts/Statistics.gd
@@ -0,0 +1,20 @@
+extends Node
+@export var money = 10
+@export var beetSeeds = 3
+@export var caulSeeds = 0
+@export var garlSeeds = 0
+@export var manSeeds = 0
+@export var milkSeeds = 0
+@export var potaSeeds = 0
+@export var radiSeeds = 0
+@export var tomaSeeds = 0
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
